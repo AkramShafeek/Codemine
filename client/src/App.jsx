f359d57b83255b115125c0c883f85da0e5be8b4d
Akram <akramshafeek70@gmail.com>
2023-11-16 23:21:57 +0530

    Initiated frontend development

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-tailwind/react"
+
+function App() { 
+  return (
+    <>
+      <h1 className="text-xl font-bold text-red-500">Hello World</h1>
+      <Button>button</Button>
+    </>
+  )
+}
+
+export default App
